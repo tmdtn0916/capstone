ce4643a3a9b8706dd8fa2a3922d123ad6406eec1
tmdtn0916 <tmdtn0916@gmail.com>
2024-06-29 11:46:43 +0900

    add user table

--- api/models.py
+++ api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 class StoryRequest(models.Model):
     keywords = models.TextField()
@@ -6,3 +8,15 @@ class StoryRequest(models.Model):
 
     def __str__(self):
         return self.keywords
+
+class User(models.Model):
+    email = models.EmailField(primary_key=True)
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=128)  # password 해싱을 위해 충분한 길이 설정
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.email
+    
+    class Meta:
+        db_table = 'user'
