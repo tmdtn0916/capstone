deaca08e920f9e61fbbaa3b62bbd3403eddc5adc
tmdtn0916 <tmdtn0916@gmail.com>
2024-10-24 13:29:20 +0900

    그림생성기능 추가

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StoryRequestViewSet, user_signup, user_login
+from .views import StoryRequestViewSet, user_signup, user_login, plot_view
 
 router = DefaultRouter()
 router.register(r'story_requests', StoryRequestViewSet)
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('storyrequest/generate_story/', StoryRequestViewSet.as_view({'post': 'generate_story'}), name='generate_story'),
     path('user/join', user_signup, name='user_signup'),
-    path('user/login', user_login, name='user_login')
+    path('user/login', user_login, name='user_login'),
+    path('plot', plot_view, name='make_plot')
 ]
--- api/views.py
+++ api/views.py
@@ -7,6 +7,7 @@ from openai import OpenAI
 import openai, logging
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
+import urllib.request
 
 logger = logging.getLogger(__name__)
 
@@ -94,3 +95,72 @@ def user_login(request):
         return Response({'token' : token.key}, status=status.HTTP_200_OK)
     else:
         return Response({'error' : 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['POST'])
+def plot_view(request):
+    if request.method == 'POST':
+
+        keywords = request.data.get('storyIdea')
+        client = OpenAI(
+        api_key=openai.api_key,
+        )
+
+        # few-shot 프롬프트 엔지니어링 적용
+        example_stories = [
+            {
+                "keywords": "고양이, 모험, 우정",
+                "story": "옛날 옛적에, 모험을 좋아하는 고양이 한 마리가 있었습니다. \n그 고양이는 친구들과 함께 숲을 탐험하며 많은 모험을 했습니다. \n그들은 서로 도우며 우정을 쌓아갔습니다."
+            },
+            {
+                "keywords": "토끼, 마법, 용기",
+                "story": "한 작은 마을에 용감한 토끼가 살고 있었습니다.\n 어느 날, 마을에 마법의 문제가 생겼습니다. \n토끼는 용기를 내어 마법을 풀기 위해 모험을 떠났고, 결국 마을을 구했습니다."
+            }
+        ]
+
+        # 프롬프트 생성
+        example_prompts = "\n".join(
+            f"Keywords: {example['keywords']}\nStory: {example['story']}\n"
+            for example in example_stories
+        )
+        
+        # 사용자 입력 키워드 추가
+        prompt = f"""You are the best children's book writer.
+            Create a children's story in Korean with these keywords: {keywords}.
+            The story should be concise and no longer than 100 characters.
+            {example_prompts}
+            Keywords: {keywords}
+            Story: """
+
+        try:
+            response =client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are the best children's book writer."},
+                    {"role": "user", "content": prompt}
+                ],
+                max_tokens=300
+            )
+            # choice = response['choices'][0]
+            # story = choice['message']['content']
+            story = response.choices[0].message.content
+            print("story: \n"+story)            
+
+            response2 = client.images.generate(
+                model="dall-e-3",
+                prompt=story,
+                size="1024x1024",
+                quality="standard",
+                n=1,
+            )
+            image_url = response2.data[0].url
+            img_dest = "./images/"
+            urllib.request.urlretrieve(image_url, img_dest+"result.jpg")
+            
+            print("---------------------- return --------------------")
+            return Response({"story": story}, status=status.HTTP_200_OK)
+        except Exception as e:
+            print(e)
+            return Response({'error': str(e)}, status=500)
+
+        # return Response({"message": "Story created successfully"}, status=status.HTTP_201_CREATED)
